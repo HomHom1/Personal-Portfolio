7e995ee7dd274c5f368ca3191d9664533190a637
HomHom1 <homahomen2@gmail.com>
2022-07-03 06:56:59 -0500

    Recent changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,9 +4,9 @@ import './nav.css'
 const Nav = () => {
     return (
     <nav>
-          <a href='/#' className='btn btn-primary btn_home'>Home</a>
-          <a href='#about' className='btn btn-primary btn_aboutme'>About Me</a>      
-          <a href='#projects' className='btn btn-primary btn_projects'>Projects</a>
+          <a href='/#' className='btn-primary btn_home'>Home</a>
+          <a href='#about' className='btn-primary btn_aboutme'>About Me</a>      
+          <a href='#projects' className='btn-primary btn_projects'>Projects</a>
     </nav>
     )
 }
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -9,12 +9,19 @@ const projects = () => {
     <section id='projects' className='projects'>
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <h3>DJ Discord Bot</h3>
+          <h3>Advanced Discord Bot</h3>
           <div className='portfolio__item-image'>
             <img src={IMG1} alt="" />
           </div>
-          <a rel="noopener noreferrer" href="https://github.com/HomHom1/DISCORD-BOT" className='btn' target='_blank'>Github</a>
-          <p>Discord bot made with Java only</p>
+          <div className='content'>
+              <div className='buttons'>
+                <a rel="noopener noreferrer" href="https://github.com/HomHom1/discordBotPython" className='btn' target='_blank'>Github</a>
+                <a rel='noopener noreferrer' href='https://discord.com/api/oauth2/authorize?client_id=533477025449771031&permissions=8&scope=bot' className='btn-primary' target='_blank'>Test</a>
+              </div>
+            <p>Discord bot made with primarily DiscordPy</p>
+            <p>(Hit the test button to try me out in your own discord server!)</p>
+
+          </div>
         </article>
 
         <article className='portfolio__item'>
@@ -22,7 +29,7 @@ const projects = () => {
           <div className='portfolio__item-image'>
             
           </div>
-          <a href="https://github.com" rel="noopener noreferrer" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com" rel="noopener noreferrer" className='btn-primary' target='_blank'>Github</a>
         </article>
 
         <article className='portfolio__item'>
@@ -30,33 +37,8 @@ const projects = () => {
           <div className='portfolio__item-image'>
 
           </div>
-          <a href="https://github.com" rel="noopener noreferrer" className='btn' target='_blank'>Github</a>
+          <a href="https://github.com" rel="noopener noreferrer" className='btn-primary' target='_blank'>Github</a>
         </article>
-
-        <article className='portfolio__item'>
-          <h3>Project 4</h3>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <a href="https://github.com" rel="noopener noreferrer" className='btn' target='_blank'>Github</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <h3>Project 5</h3>
-          <div className='portfolio__item-image'>
-
-          </div>
-          <a href="https://github.com" rel="noopener noreferrer" className='btn' target='_blank'>Github</a>
-        </article>
-
-        <article className='portfolio__item'>
-          <h3>Project 6</h3>
-          <div className='portfolio__item-image'>
-
-          </div>
-          <a href="https://github.com" rel="noopener noreferrer" className='btn' target='_blank'>Github</a>
-        </article>
-
       </div>
     </section>
   )
